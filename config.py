1e18b57fc673d72978f1721bfb0be45a8bdda508
Yuzi-Liang <liang.yuzi@northeastern.edu>
2025-10-24 10:10:43 -0700

    fix: 修复若干资源释放问题

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-from astrbot.api.star import StarTools
+import os
 
-TEMP_DIR = str(StarTools.get_data_dir() / "temp")  # 临时目录
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ROOT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+
+TEMP_DIR = os.path.join(ROOT_DIR, "temp", "mirage_tank_cache")  # 临时目录
